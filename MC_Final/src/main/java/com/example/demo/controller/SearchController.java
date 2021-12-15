063f4cc325743fe46ba1844e9c4789bcae432536
HyeonHo Yang <91450728+Yangdoubleh@users.noreply.github.com>
2021-12-15 17:22:44 +0900

    food

--- MC_Final/src/main/java/com/example/demo/controller/SearchController.java
+++ MC_Final/src/main/java/com/example/demo/controller/SearchController.java
@@ -2,10 +2,13 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.json.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.example.demo.service.ReceipeSerachService;
 import com.example.demo.service.TranslationService;
@@ -21,9 +24,19 @@ public class SearchController {
 	
 	@RequestMapping("receipeSearch")
 	@ResponseBody
-	public String receipeSearch(String ingredient) {
+	public String receipeSearch(String ingredient) throws ParseException {
 		//외국 API를 사용하여 식재료를 영어로 변환 후 사용
-		return receipeSearchService.ReceipeSearch(translationService.translatekoen(ingredient));
+		String strJson = receipeSearchService.ReceipeSearch(translationService.translatekoen(ingredient));
+		JSONParser jsonParser= new JSONParser();
+		Object obj = jsonParser.parse(strJson);
+		JSONObject jo1 = (JSONObject)obj;
+		JSONArray ja1 = (JSONArray)jo1.get("hits");
+		for(int i=0; i<19; i++) {
+			JSONObject jo2 = (JSONObject) ja1.getJSONObject(i);
+			jo2.replace();
+		}
+		return ;
+		
 		//return => 음식 이름 / 사진 / 레시피 URL / kcal / 탄수화물 / 단백질 / 지방
 	}
 	
